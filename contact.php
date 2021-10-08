ea6cc2fe552d25e8f1cb8cd8ce8e387399bf538f
nanudmr <nanudmr@gmail.com>
2021-10-08 09:41:21 +0100

    123

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo = "info@nakecode.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $txt, $headers);
+
+    header("Location: index.php?mailsend");
+}
---
+++ index.js
@@ -0,0 +1,80 @@
+
+// MENU MENU MENU MENU MENU MENU MENU MENU MENU MENU MENU 
+
+const menuOpener = document.getElementById("openMenu")
+
+const menuCloser = document.getElementById("closeMenu")
+
+const menu = document.getElementById("menu")
+
+menuOpener.addEventListener("click", function(){
+    menu.style.display = "block"
+    menuOpener.style.display = "none"
+    menuCloser.style.display = "block"
+})
+
+menuCloser.addEventListener("click", function(){
+    menu.style.display = "none"
+    menuOpener.style.display = "block"
+    menuCloser.style.display = "none"
+})
+
+// MENU MENU MENU MENU MENU MENU MENU MENU MENU MENU MENU 
+
+
+
+
+// CAROUSEL CAROUSEL CAROUSEL CAROUSEL CAROUSEL 
+
+var splide = new Splide( '.splide' );
+
+splide.mount();
+
+
+function carouselShow(x) {
+  if (x.matches) { 
+    let splide = new Splide( '.splide' );
+    splide.mount();
+  } else {
+    let splide = new Splide( '.splide', {
+      perPage: 3,
+      rewind : true,
+    } );
+    
+    splide.mount();
+  }
+}
+
+let space = window.matchMedia("(max-width: 809px)")
+carouselShow(space)
+space.addListener(carouselShow)
+
+
+
+// CAROUSEL CAROUSEL CAROUSEL CAROUSEL CAROUSEL 
+
+
+
+
+// FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
+
+
+
+
+
